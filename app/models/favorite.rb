4dd8dcf6d217c7e73a78d49d87d7dcb157735765
Raghu Betina <raghu@firstdraft.com>
2016-12-05 00:32:06 +0000

    Validate uniqueness of favorites.news_source_id

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -5,6 +5,8 @@ class Favorite < ApplicationRecord
 
   # Validations
 
+  validates :news_source_id, :uniqueness => { :scope => [:user_id] }
+
   validates :news_source_id, :presence => true
 
   validates :user_id, :presence => true
