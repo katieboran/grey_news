7ea75384618b6049b91a4eefcfd578d56b7e24a8
Raghu Betina <raghu@firstdraft.com>
2016-12-05 00:33:00 +0000

    Whitelist columns in BiasRating for ActiveAdmin

--- app/admin/bias_rating.rb
+++ app/admin/bias_rating.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register BiasRating do
 
+ permit_params :user_id, :news_source_id, :rating
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
