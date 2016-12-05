da43d1c979c7ba7194e554caf1c9de3b3bff70d5
Raghu Betina <raghu@firstdraft.com>
2016-12-05 00:32:06 +0000

    Validate presence of comments.content

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,6 +5,8 @@ class Comment < ApplicationRecord
 
   # Validations
 
+  validates :content, :presence => true
+
   validates :news_source_id, :presence => true
 
   validates :user_id, :presence => true
